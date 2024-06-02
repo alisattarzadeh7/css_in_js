b62bec04c394c1168f3e9566bda78160df821d01
Ali Sattarzadeh <alisattarzadeh71@gmail.com>
2024-06-02 22:15:29 +0330

    feat: adding husky, stagelint, emotion and styled-component to project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+
+function App() {
+    return (
+        <div className="main">
+
+        </div>
+    )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({
+    jsxImportSource: "@emotion/react",
+    babel: {
+      plugins: ["@emotion/babel-plugin"],
+    },
+  }),eslint()],
+})
